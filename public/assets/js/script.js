d3bb5df14beed72cfdc7763893821dc391ff5f64
pmaganty <pranithamaganty@gmail.com>
2020-03-28 14:56:23 -0500

    trying to get latest

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,25 +3,13 @@ $(document).ready(function() {
 // When the user clicks on Found a pet, direct them to fill a form
 $("#foundbtn").click(function(){
   location.replace("/foundPets");
-
-    /*$.ajax({
-        method: "GET",
-        url: "/foundPets"
-      })
-        .then(function(response) {
-            console.log(response);
-            console.log("button clicked!");
-
-        });*/
-    
-  });
-
-
+});
 
  // When user clicks on lost a pet, direct them to fill a form 
  $("#lostbtn").click(function(){
     location.replace("/lostPets");
   });
-   
 
+
+   
 });
